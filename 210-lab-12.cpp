a04af710f95c893a9794738433478661037a886c
Gabe Marquez <gabrielmarquez98@gmail.com>
2026-02-22 11:45:24 -0800

    update

--- 210-lab-12.cpp
+++ 210-lab-12.cpp
@@ -16,7 +16,8 @@ const int SIZE = 35;
 int main() {
     //declare std::array of SIZE 35 via reading data from a file,
     //using .size() to loop through the array and .at(i) to
-    // populate each element
+    // populate each element and confirming with .empty()
+    cout << "Populating runTimes array..." << endl;
     array<double, SIZE> runTimes;
     ifstream fin("run_times.txt");
     if (fin.good()) {
@@ -28,8 +29,11 @@ int main() {
         cout << "ERROR! Please verify file name/directory and restart program.";
         return 1;
     }
+    if (!runTimes.empty())
+        cout
 
     //loop to display elements in runTimes
+    cout << "10 kilometer run times in minutes from the past 35 days:" << endl;
     for (int i = 0; i < runTimes.size(); ++i) {
         cout << runTimes[i]; 
         if (i < runTimes.size() - 1)
