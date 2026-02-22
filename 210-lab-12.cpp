17d6b9048512558316255bba42912aa80e95deb2
Gabe Marquez <gabrielmarquez98@gmail.com>
2026-02-22 11:04:35 -0800

    Initial commit: starting lab 12

---
+++ 210-lab-12.cpp
@@ -0,0 +1,13 @@
+//*****************************************************************************
+// COMSC-210 | Lab 12 | Gabriel Marquez
+// Description: this program exercises a variety of std::array functions by
+// tracking 10 kilometer run times over a span of 35 days.
+//*****************************************************************************
+
+#include <iostream>
+#include <algorithm>  // for sort(), find()
+#include <numeric>    // for accumulate()
+#include <array>
+#include <fstream>
+using namespace std;
+
