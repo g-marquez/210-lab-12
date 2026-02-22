ce6047239291d64df1406eb0a107fc70a252a99c
Gabe Marquez <gabrielmarquez98@gmail.com>
2026-02-22 11:14:54 -0800

    update

--- 210-lab-12.cpp
+++ 210-lab-12.cpp
@@ -11,3 +11,22 @@
 #include <fstream>
 using namespace std;
 
+const int SIZE = 35;
+
+int main() {
+    //declare std::array of SIZE 35 via reading data from a file,
+    //using .size() to loop through the array and populate
+    array<double, SIZE> runTimes;
+    ifstream fin("run_times.txt");
+    if (fin.good()) {
+        for (int i = 0; i < runTimes.size(); ++i)
+            fin >> runTimes[i];
+        fin.close();
+    }
+    else {
+        cout << "ERROR! Please verify file name/directory and restart program.";
+        return 1;
+    }
+
+    return 0;
+}
